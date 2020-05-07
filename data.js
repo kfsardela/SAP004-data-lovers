068a246d52ae336e33375ffc2cf118947640498f
Karine Sardela <kfsardela@gmail.com>
2020-05-06 21:38:13 -0300

    Updates

--- data.js
+++ data.js
@@ -1,16 +1,31 @@
-// funções de exemplo
-
-/*filterData(data, condition): esta função receberia os dados e nos retornaria os que cumprem com a condição.
-
-sortData(data, sortBy, sortOrder): esta função recebe três parâmetros. O primeiro, data, nos entrega os dados. O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, indica se quer ordenar de maneira crescente ou decrescente.
-
-computeStats(data): essa função nos permite fazer cálculos estatísticos básicos para serem exibidos de acordo com o que os dados permitem.*/
-
-
-export const example = () => {
-  return 'example';
+export const filtro = {
+  filterByType(data, args) {
+    return data.filter((poke) => {
+      return poke.type[0] === args;
+    });
+  },
+  filterByEgg(data, args) {
+    return data.filter((poke) => {
+      return poke.egg === args;
+    });
+  }
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const sortData = (data, sortBy, sortOrder) => {
+  data.sort(function (a, b) {
+    if (a[sortBy] > b[sortBy]) {
+      return 1;
+    }
+    if (a[sortBy] < b[sortBy]) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  });
+
+  if (sortOrder === "desc") {
+    data.reverse()
+  }
+
+  return data
+}
